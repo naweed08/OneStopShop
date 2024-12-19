4d99554f8692ea59dfca87b77bac630f41925356
naweed08 <naweedshah08@gmail.com>
2024-12-19 17:08:00 +0000

    Completed Products Class with required methods

--- oneStopShop/src/Product.java
+++ oneStopShop/src/Product.java
@@ -27,4 +27,31 @@ public class Product {
         this.quantity = quantity;
     }
 
+    /** Changes the price of a product if required
+     * later on.
+     * @param price - changes set price at initialization to new price
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    /** Returns the products price
+     *
+     * @return the current product price
+     */
+    public double getPrice(){
+        return this.price;
+    }
+
+    /** Returns the subTotal for each product,
+     * For example, customers buys 3 apples, each apple
+     * costs 1.20. The subTotal would be calculated by
+     * multiplying the price by the quantity.
+     *
+     * @return the subtotal of current scanned Products at
+     * checkout
+     */
+    public double calculateSubTotal(){
+       return this.subTotal = this.price * this.quantity;
+    }
 }
