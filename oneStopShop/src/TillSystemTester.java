43e058e1a013d8c9e50003d6c5917fc8824cc321
naweed08 <naweedshah08@gmail.com>
2024-12-25 23:58:04 +0000

    Created tester class for Till system class

--- oneStopShop/src/TillSystemTester.java
+++ oneStopShop/src/TillSystemTester.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class TillSystemTester {
+public class TillSystemTester {
+
+    public static void main(String[] args) {
+        TillSystem ts = new TillSystem();
+        Product banana = new Product("Banana", 0.99, 6);
+
+        ts.addProduct(banana);
+
+        System.out.println(ts.displayScannedProducts());
+
+    }
+
+
 }
