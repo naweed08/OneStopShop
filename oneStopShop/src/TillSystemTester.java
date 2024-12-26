0a9e84c00eacdeae3118815e978bcca01ff3f8a6
naweed08 <naweedshah08@gmail.com>
2024-12-26 00:33:01 +0000

    Added some print statement labels to understand what tests are being carried out

--- oneStopShop/src/TillSystemTester.java
+++ oneStopShop/src/TillSystemTester.java
@@ -3,10 +3,27 @@ public class TillSystemTester {
     public static void main(String[] args) {
         TillSystem ts = new TillSystem();
         Product banana = new Product("Banana", 0.99, 6);
+        Product carrot = new Product("Carrots", 1.29, 4);
+        Product mango = new Product("Mango", 0.49, 12);
 
         ts.addProduct(banana);
 
-        System.out.println(ts.displayScannedProducts());
+        // Note each time printing the scanned products prints entire list:
+        System.out.println("Added banana: " + ts.displayScannedProducts());
+
+        ts.addProduct(carrot);
+
+        System.out.println("Added carrot: " + ts.displayScannedProducts());
+
+        ts.removeProduct(banana);
+
+        System.out.println("Removed banana: " + ts.displayScannedProducts());
+
+        ts.addProduct(mango);
+
+        System.out.println("Added mango: " + ts.displayScannedProducts());
+
+        System.out.println(ts.calculateTotal());
 
     }
 
