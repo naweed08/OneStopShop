b61c47e8f9fd52c6b84fd32afcf9f53750319522
naweed08 <naweedshah08@gmail.com>
2025-02-19 21:43:35 +0000

    Objects in tester class required values of LocalDate type so added sample expiry dates

--- oneStopShop/src/TillSystemTester.java
+++ oneStopShop/src/TillSystemTester.java
@@ -1,10 +1,12 @@
+import java.time.LocalDate;
+
 public class TillSystemTester {
 
     public static void main(String[] args) {
         TillSystem ts = new TillSystem();
-        Product banana = new Product("Banana", 0.99, 6);
-        Product carrot = new Product("Carrots", 1.29, 4);
-        Product mango = new Product("Mango", 0.49, 12);
+        Product banana = new Product("Banana", 0.99, 6, LocalDate.of(2025, 8, 1));
+        Product carrot = new Product("Carrots", 1.29, 4, LocalDate.of(2025, 8, 1));
+        Product mango = new Product("Mango", 0.49, 12, LocalDate.of(2025, 8, 1));
 
         ts.addProduct(banana);
 
