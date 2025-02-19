2b30f38cd4ec50dd96f57bc291188927b312e294
naweed08 <naweedshah08@gmail.com>
2025-02-19 21:44:29 +0000

    Added LocalDate parameter to constructor and super() to track product expiry dates

--- oneStopShop/src/Tobacco.java
+++ oneStopShop/src/Tobacco.java
@@ -1,20 +1,21 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Tobacco extends Product {
 
     private ArrayList<Product> tobaccoList;
 
-    public Tobacco(String description, double price, int quantity) {
-        super(description, price, quantity);
+    public Tobacco(String description, double price, int quantity, LocalDate expiryDate) {
+        super(description, price, quantity, expiryDate);
         tobaccoList = new ArrayList<>();
         initializeTobaccoList();
     }
 
     private void initializeTobaccoList() {
-        tobaccoList.add(new Product("Marlboro", 8.99, 20));
-        tobaccoList.add(new Product("Blue Light", 12.99, 12));
-        tobaccoList.add(new Product("Buzz Light", 6.99, 24));
-        tobaccoList.add(new Product("Grey Goose", 11.99, 17));
+        tobaccoList.add(new Product("Marlboro", 8.99, 20, LocalDate.of(2025, 8, 1)));
+        tobaccoList.add(new Product("Blue Light", 12.99, 12, LocalDate.of(2025, 8, 1)));
+        tobaccoList.add(new Product("Buzz Light", 6.99, 24, LocalDate.of(2025, 8, 1)));
+        tobaccoList.add(new Product("Grey Goose", 11.99, 17, LocalDate.of(2025, 8, 1)));
     }
 
     public String getTobaccoList () {
