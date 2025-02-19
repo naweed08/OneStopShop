feff7b12c1c241994af3330c041b46ef05429855
naweed08 <naweedshah08@gmail.com>
2025-02-19 21:26:26 +0000

    Added LocalDate functionality to drinks class to allow staff to set and view expiry date of drinks

--- oneStopShop/src/Drinks.java
+++ oneStopShop/src/Drinks.java
@@ -1,20 +1,21 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Drinks extends Product {
 
     private ArrayList<Product> drinkList;
 
-    public Drinks(String description, double price, int quantity) {
-        super(description, price, quantity);
+    public Drinks(String description, double price, int quantity, LocalDate expiryDate) {
+        super(description, price, quantity, expiryDate);
         drinkList = new ArrayList<>();
         initializeDrinkList();
     }
 
     private void initializeDrinkList() {
-        drinkList.add(new Product("Coca Cola", 0.79, 34));
-        drinkList.add(new Product("Pepsi Max", 0.79, 14));
-        drinkList.add(new Product("Lucozade", 1.29, 23));
-        drinkList.add(new Product("Red Bull", 1.79, 18));
+        drinkList.add(new Product("Coca Cola", 0.79, 34, LocalDate.of(2025, 8, 1)));
+        drinkList.add(new Product("Pepsi Max", 0.79, 14, LocalDate.of(2026, 8, 1)));
+        drinkList.add(new Product("Lucozade", 1.29, 23, LocalDate.of(2025, 4, 16)));
+        drinkList.add(new Product("Red Bull", 1.79, 18, LocalDate.of(2025, 11, 11)));
     }
 
     public String getDrinksList() {
