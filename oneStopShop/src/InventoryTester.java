aa69caf4197f2b6a4d476df52efc2e80480cc91f
naweed08 <naweedshah08@gmail.com>
2025-03-31 12:41:57 +0100

    Created inventory object and added item to carry out two tests, will continue to test in detail

--- oneStopShop/src/InventoryTester.java
+++ oneStopShop/src/InventoryTester.java
@@ -7,6 +7,11 @@ public class InventoryTester {
 
         System.out.println("Test 1: Add crisps to inventory");
         in1.addProduct(new Product("Crisps", 0.79, 12, LocalDate.of(2025, 11, 2)));
+        System.out.println(in1.viewAllProducts());
+
+        System.out.println("Test 2: Change crisps Expiry date");
+        in1.changeDate("Crisps", LocalDate.of(2025,11,8));
+        System.out.println(in1.viewAllProducts());
     }
 
 
